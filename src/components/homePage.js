ea84eae2692c747fb3e4bf0fdb7745a6ee5d4b85
Sir Edward <edwardsotelojr@gmail.com>
2021-11-30 13:47:10 -0600

    got it to render available tables after selection

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -4,7 +4,7 @@ import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { Component } from "react";
 import TimePicker from "@mui/lab/TimePicker";
 import { getDate } from "date-fns";
 
@@ -30,6 +30,7 @@ const tables = {
   t19: 5,
   t20: 5,
 };
+
 let arrayOfTables = [
   "t1",
   "t2",
@@ -53,6 +54,127 @@ let arrayOfTables = [
   "t20",
 ];
 
+class HomePage extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      tables: [],
+      date: "",
+      time: ""
+    }
+    this.setTables = this.setTables.bind(this)
+  }
+
+  getTableList = (d, t) => {
+    var tablesAvailable = []
+    //console.log(d.value)
+    //console.log(t.value)
+    const parameters = {
+      params: {
+        date: d.value,
+        time: t.value,
+      },
+    }; 
+    if(t.value.length != 0){
+      console.log(parameters)
+     axios
+      .get("http://localhost:8000/getAvailableTables", parameters)
+      .then((res) => {
+        this.setTables(res.data.reservation)
+        //console.log("res ",res)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+    }
+  }
+
+   convertResponse(tablesTaken) {
+    var stringArray = [];
+    var str = "";
+    for (var i = 0; i < tablesTaken.length; i++) {
+      str = "t";
+      str = str + tablesTaken[i].toString();
+      stringArray.push(str);
+    }
+    return stringArray;
+  }
+
+   setTables(res){
+    var tablesAvailable = arrayOfTables;
+    tablesTaken = res;
+    var tablesTaken = this.convertResponse(tablesTaken);
+    for (var i = 0; i < tablesTaken.length; i++) {
+      // go through array of tables and remove taken tables
+      tablesAvailable = tablesAvailable.filter((ta) => ta !== tablesTaken[i]);
+    }
+    console.log(tablesAvailable)
+    //var lis = tablesAvailable.map((t,index) => 
+    //  <li key={index}>{t}</li>
+    //);
+    this.setState({tables: tablesAvailable})
+  }
+  
+  render(){
+    
+      
+
+    
+    return (
+      <Container>
+      <Row>
+        <Col>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DesktopDatePicker
+              label="Select Date"
+              value={this.state.date}
+              onChange={(newValue) => {
+                this.setState({date: newValue});
+                this.getTableList(document.getElementsByName("dateValue")[0], 
+              document.getElementsByName("timeValue")[0])
+              }}
+              renderInput={(params) => <TextField name={"dateValue"} {...params} />}
+            />
+          </LocalizationProvider>
+        </Col>
+        <Col>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <TimePicker
+              label="Select Time"
+              value={this.state.time}
+              minutesStep={15}
+              onChange={(newValue) => {
+                this.setState({time: newValue});
+                this.getTableList(document.getElementsByName("dateValue")[0], 
+              document.getElementsByName("timeValue")[0])
+              }}
+              renderInput={(params) => <TextField name={'timeValue'} {...params} />}
+            />
+          </LocalizationProvider>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        Table of Available Tables
+        </Col>
+      </Row>
+      <Row>
+        <ul>
+          {this.state.tables.map((t, index) => 
+            <li key={index}>{t}</li>
+          )}
+        </ul>
+      </Row>
+    </Container>
+    )
+  }
+}
+
+
+/*
+
+
+
 function convertResponse(tablesTaken) {
   var stringArray = [];
   var str = "";
@@ -64,14 +186,12 @@ function convertResponse(tablesTaken) {
   return stringArray;
 }
 
-
 function HomePage() {
   const [date, setDate] = useState(null);
   const [time, setTime] = useState(null);
+  const [ta, setTa] = useState(null)
 
-  const [tables, setTables] = useState(null);
-
-   function getTableList(d, t){
+   async function getTableList(d, t){
     var tablesAvailable = []
     const parameters = {
       params: {
@@ -79,31 +199,22 @@ function HomePage() {
         time: t.value,
       },
     }; 
-    const lis = <li>called</li>
     if(t.value.length != 0){
       console.log(parameters)
-    axios
+    const test = await axios
       .get("http://localhost:8000/getAvailableTables", parameters)
       .then((res) => {
-        tablesAvailable = arrayOfTables;
-        console.log(res.data.reservation)
-        tablesTaken = res.data.reservation;
-        var tablesTaken = convertResponse(tablesTaken);
-        for (var i = 0; i < tablesTaken.length; i++) {
-          // go through array of tables and remove taken tables
-          tablesAvailable = tablesAvailable.filter((ta) => ta !== tablesTaken[i]);
-        }
-        console.log(tablesAvailable)
-        lis = tablesAvailable.map((t,index) => 
-          <li key={index}>{t}</li>
-        );
+        //setTables(res.data.reservation)
+        console.log("res ",res)
       })
       .catch((err) => {
         console.log(err)
       });
+      return test
     }
-    return lis
   }
+
+  
 
 
   return (
@@ -143,8 +254,13 @@ function HomePage() {
               document.getElementsByName("timeValue")[0]) : <li>none</li>}</ul>
         </Col>
       </Row>
+      <Row>
+        <ul>
+          {ta}
+        </ul>
+      </Row>
     </Container>
   );
 }
-
+*/
 export default HomePage;
