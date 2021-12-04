78829df9bdad4bfb1f0602c76a072a94d3f66789
Elizabeth N <72176806+silverbar18@users.noreply.github.com>
2021-12-03 19:52:57 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen, fireEvent, waitFor, cleanup} from '@testing-library/react';
 import App from './App';
+import Signup from './components/Signup';
+import DateSelection from './components/DateSelection';
+import Header from './components/Header';
+import TimeSelection from './components/TimeSelection';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import '@testing-library/jest-dom';
+import HomePage from "./components/HomePage";
+import ReactDOM from "react-dom";
+
+it ("renders w/o creashing", () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Signup />, div);
+  ReactDOM.render(<HomePage />, div);
+  ReactDOM.render(<DateSelection />, div);
+  ReactDOM.render(<Header />, div);
+  ReactDOM.render(<TimeSelection />, div);
+})
